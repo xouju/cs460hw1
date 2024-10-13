1c5fbc07b2843303b845bb2b3557165d18e00f98
oxuju <naijahuney@gmail.com>
2024-10-13 12:21:07 -0700

    so basically, yesterday night, i was able to finish the javascript part of the code of getting the names to work. I didn't want to go to bed unless I finished it and it worked for the most part. I had to rely on chatgpt and some videos and I also used stack overflow for some help. I am not very good with javascript, so i struggled the most there. I am very glad i finished that yesterday so now i can just focus on the unittesting which isn't as bad as the javascript. also, with the unittest, i have struggled all morning wondering why everything was red, but then i realized that perhaps there is a video i could watch because i haven't done unittesting in a while and I figured it out in a minute and there is no more red and I am exhausted. I do have ideas for what I want to test for the unittest. i want to test out the names (entering the names of the people), the teams (the max amount of teams a person can generate), i also want to test if the names are not illegal and for the last test, i will probably test if the names of the teams do change upon clicking the button.

---
+++ HW1.Unittest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HW1.Unittest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
--- cs460hw1/Controllers/HomeController.cs
+++ cs460hw1/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using cs460hw1.Models;
 using System.Linq; // Make sure to include this for LINQ methods
+using System;
+using System.Text;
 
 namespace cs460hw1.Controllers
 {
@@ -14,67 +16,85 @@ namespace cs460hw1.Controllers
             _logger = logger;
         }
 
+        // Displays the index view
         public IActionResult Index()
         {
             return View();
         }
 
+        // Displays the privacy view
         public IActionResult Privacy()
         {
             return View();
         }
 
+        // Displays the team creator view
         public IActionResult TeamCreator()
         {
-            return View(new TeamCreatorViewModel { Names = string.Empty});
+            return View(new TeamCreatorViewModel { Names = string.Empty });
         }
 
-[HttpPost]
-public IActionResult GenerateTeams(TeamCreatorViewModel model)
-{
-    Debug.WriteLine($"Entered Names: {model.Names}");
+        // Handles form submission to generate teams
+        [HttpPost]
+        public IActionResult GenerateTeams(TeamCreatorViewModel model)
+        {
+            Debug.WriteLine($"Entered Names: {model.Names}");
 
-    // Validate model state; this will capture required and range validation.
-    if (!ModelState.IsValid)
-    {
-        return BadRequest(ModelState); // Return validation errors
-    }
+            // Validate model state; this will capture required and range validation.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); // Return validation errors
+            }
 
-    // Process names into a list
-    var namesList = model.Names
-                         .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
-                         .Select(name => name.Trim())
-                         .ToList();
+            // Process names into a list
+            var namesList = model.Names
+                                 .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(name => name.Trim())
+                                 .ToList();
 
-    // Generate teams based on the number of teams requested
-    model.Teams = CreateTeams(namesList, model.NumTeams);
+            // Generate teams based on the number of teams requested
+            model.Teams = CreateTeams(namesList, model.NumTeams);
 
-    return PartialView("_TeamsPartial", model); // Return the view with the generated teams
-}
+            // Create HTML string for the generated teams
+            var html = new StringBuilder();
+            for (int i = 0; i < model.NumTeams; i++)
+            {
+                html.Append($"<div class='team'><h5 class='team-name'>Team {i + 1}</h5><ul>");
+                foreach (var member in model.Teams[i])
+                {
+                    html.Append($"<li>{member}</li>");
+                }
+                html.Append("</ul></div>");
+            }
 
-private List<List<string>> CreateTeams(List<string> names, int numTeams)
-{
-    var teams = new List<List<string>>(numTeams);
-    
-    // Initialize empty lists for each team
-    for (int i = 0; i < numTeams; i++)
-    {
-        teams.Add(new List<string>());
-    }
+            return Content(html.ToString(), "text/html"); // Return the generated HTML
+        }
 
-    // Shuffle the names randomly
-    var random = new Random();
-    var shuffledNames = names.OrderBy(x => random.Next()).ToList();
+        // Creates evenly distributed teams from the list of names
+        private List<List<string>> CreateTeams(List<string> names, int numTeams)
+        {
+            var teams = new List<List<string>>(numTeams);
+            
+            // Initialize empty lists for each team
+            for (int i = 0; i < numTeams; i++)
+            {
+                teams.Add(new List<string>());
+            }
 
-    // Distribute the names across the teams
-    for (int i = 0; i < shuffledNames.Count; i++)
-    {
-        teams[i % numTeams].Add(shuffledNames[i]);
-    }
+            // Shuffle the names randomly
+            var random = new Random();
+            var shuffledNames = names.OrderBy(x => random.Next()).ToList();
 
-    return teams;
-}
+            // Distribute the names across the teams
+            for (int i = 0; i < shuffledNames.Count; i++)
+            {
+                teams[i % numTeams].Add(shuffledNames[i]);
+            }
 
+            return teams;
+        }
+
+        // Handles error responses
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
