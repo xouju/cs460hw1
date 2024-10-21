461d0ef3dd051180e6095fa860bb56322ff0eb0d
oxuju <naijahuney@gmail.com>
2024-10-20 20:27:42 -0700

    originally i was going to uoload this sooner because i finished the homework on time and everything, but uh.... the program wasn't working because of a name error i might've overlooked even though it was working as i showed in the video so i was extremely confused. also, i just deleted a script i didn't need because after using chagpt for some help, i got things fixed and i figured i didn't need it in the end. so i believe everything should be working properly now.

--- cs460hw1/obj/Debug/net8.0/cs460hw1.AssemblyInfo.cs
+++ cs460hw1/obj/Debug/net8.0/cs460hw1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs460hw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5fbc07b2843303b845bb2b3557165d18e00f98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b53a95de25eaf8f9f6b7402ccd8421dcb2d275")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs460hw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs460hw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
